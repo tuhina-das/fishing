45adbaa2406d778e279795c9b3ea70bf04a051fc
Tuhina <114300168+tuhina-das@users.noreply.github.com>
2026-01-07 01:06:09 -0600

    Added fishies and water yay

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,113 @@
  * Note: This is my first time using ThreeJS, and my first React project in a minute! I'm going to be making notes as I go to
  * help me catch up to speed :)
  *
- * TODO: Create dynamic lighting and disappearing faces.
+ * TODO: Clone fish and animate them a lil more
  */
+import * as THREE from "three";
 
-import { OrbitControls, useTexture } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  Sky,
+  useTexture,
+  MeshTransmissionMaterial,
+  Environment,
+} from "@react-three/drei";
+import { Canvas, useFrame, useThree, useLoader } from "@react-three/fiber";
 import { useRef } from "react";
-import { CylinderGeometry, PointLight } from "three";
+import { CylinderGeometry, FrontSide, PointLight } from "three";
 import { useTextures } from "./Textures.jsx";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
-// Creating a rotating cube object. Involves useRef to get a reference to the specific mesh, a geometry object, and a material.
-// UseRef ensures the mesh stays constant.
+// Mesh for the sphere container
 const SphereContainer = () => {
-  const meshRef = useRef();
-  const { tile } = useTextures();
   return (
-    <mesh ref={meshRef}>
-      <sphereGeometry arge={[1, 1, 1]} />
-      <meshLambertMaterial map={tile} />
+    <mesh>
+      <sphereGeometry args={[5, 64, 64]} />
+      <MeshTransmissionMaterial
+        transmission={1}
+        roughness={0}
+        thickness={0.2}
+        chromaticAberration={0.05}
+        anisotropy={0.1}
+        distortion={0.05}
+        distortionScale={0.05}
+        temporalDistortion={0}
+        side={FrontSide}
+        emmissiveIntensity={0.5}
+        emmissiveColor={"#006361"}
+      />
     </mesh>
   );
 };
 
-const App = () => {
+// Mesh for the water inside the sphere container
+const WaterMaterial = () => {
+  return (
+    <mesh>
+      <sphereGeometry args={[4.75, 64, 64]} />
+      <meshPhysicalMaterial
+        transparent
+        opacity={0.7}
+        color="#27BEF5"
+        roughness={0}
+        metalness={0}
+        ior={1.33}
+        attenuationColor="#27BEF5"
+        transmission={1}
+        thickness={1.5}
+        attenuationDistance={1}
+      />
+    </mesh>
+  );
+};
+
+// Dynamic lighting woooo! -- light that follows the camera for when we want the sphere to be opaque
+function CameraLight() {
+  const { camera } = useThree(); // Grab camera reference from scene
+  const lightRef = useRef(); // Then create a reference for the light so we can update its position
+
+  useFrame(() => {
+    // For every frame, if the light is there, update the light's position
+    if (lightRef.current) {
+      lightRef.current.position.copy(camera.position);
+    }
+  });
+
+  return <pointLight ref={lightRef} intensity={20} color={0xffffff} />;
+}
+
+const Fish = () => {
+  // Render the fish
+  const fish = useLoader(OBJLoader, "../public/assets/fish.obj");
+  const texture = useLoader(THREE.TextureLoader, "../public/assets/fish.jpg");
+  fish.rotation.x = Math.PI / 2;
+  fish.rotation.y = Math.PI;
+  fish.position.x = -0.5;
+  fish.position.y = -0.5;
+  fish.traverse((child) => {
+    if (child.isMesh) {
+      child.material = new THREE.MeshStandardMaterial({
+        map: texture,
+      });
+    }
+  });
+
+  // Animate the fish
+  useFrame(() => {
+    fish.position.y += Math.sin(Date.now() * 0.001) * 0.01;
+  });
+
+  return (
+    <>
+      <primitive object={fish} scale={0.1} />
+    </>
+  );
+};
+
+function App() {
   return (
     <Canvas
+      // TODO: eventually add TWCSS possibly
       style={{
         height: "100vh",
         width: "100vw",
@@ -36,19 +118,27 @@ const App = () => {
         alignItems: "center",
       }}
     >
+      <Environment preset="city" />
+      {/* Skybox for background environment */}
+      <Sky
+        distance={10000000}
+        sunPosition={[0, 100, 0]}
+        inclination={0}
+        azimuth={0.25}
+      />
+
       {/* Controls for the camera */}
       <OrbitControls enableZoom enablePan enableRotate />
-
-      {/* Scene lighting */}
-      <directionalLight position={[1, 1, 1]} intensity={15} color={0x9cdba6} />
 
       {/* Background color */}
       <color attach="background" args={["#000000"]} />
 
       {/* The actual mesh for water container.*/}
       <SphereContainer />
+      <WaterMaterial />
+      <Fish />
     </Canvas>
   );
-};
+}
 
 export default App;
--- src/Textures.jsx
+++ src/Textures.jsx
@@ -2,5 +2,6 @@ import { useTexture } from "@react-three/drei";
 
 export const useTextures = () => {
   const tile = useTexture("/assets/tile.jpg");
-  return { tile };
+  const sky = useTexture("/assets/sky.jpeg");
+  return { tile, sky };
 };
